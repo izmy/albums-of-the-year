ac47bd260ebb7d1994387198b94b73b6f90bed9a
Jaroslav Hrách <me@jaroslavhrach.cz>
2020-12-27 14:09:55 +0100

    improve change

--- src/routes/Change/Change.tsx
+++ src/routes/Change/Change.tsx
@@ -1,14 +1,22 @@
-import { TextField } from "@material-ui/core";
 import * as React from "react";
+import styled from "styled-components";
+import { TextField } from "@material-ui/core";
 import { LoadingSpinner } from "../../components/LoadingSpinner";
 import { UserList } from "../../models/user.types";
 import { Vote } from "../../models/votes.types";
 import { getAllUsers } from "../../services/api/usersApi";
 import { getAllVotes, updateVote } from "../../services/api/votesApi";
-import { ChangeList } from "./ChangeList";
+import { ChangeItem } from "./ChangeItem";
+
+const FilterTextField = styled(TextField)`
+  width: 100%;
+  max-width: 500px;
+  margin-bottom: 50px;
+`;
 
 export const Change: React.FC = () => {
   const [votes, setVotes] = React.useState<Vote[]>([]);
+  const [filteredVotes, setFilteredVotes] = React.useState<Vote[]>([]);
   const [users, setUsers] = React.useState<UserList>({});
   const [loading, setLoading] = React.useState(true);
   const [filter, setFilter] = React.useState("");
@@ -17,6 +25,7 @@ export const Change: React.FC = () => {
     const fetchData = async () => {
       const allVotes = await getAllVotes();
       setVotes(allVotes.data);
+      setFilteredVotes(allVotes.data);
 
       const allUsers = await getAllUsers();
       const allUsersInObject = allUsers.data.reduce((acc, curr) => {
@@ -32,51 +41,68 @@ export const Change: React.FC = () => {
     fetchData();
   }, []);
 
-  const handleSaveVote = (vote: Vote) => {
+  const filterVotes = React.useCallback(
+    (filteredValue: string, sourceVotes: Vote[]) => {
+      const filteredVotes = sourceVotes.filter((vote) => {
+        const value = `${vote.artist} ${vote.album}`.toLowerCase();
+        return value.includes(filteredValue);
+      });
+      setFilteredVotes(filteredVotes);
+    },
+    []
+  );
+
+  const handleSaveVote = React.useCallback((vote: Vote) => {
     if (vote._id && window.confirm("Chcete uložit změny?")) {
       updateVote(vote._id, vote);
     }
-  };
-
-  const handleChangeVotes = React.useCallback((newVote: Vote) => {
-    setVotes((prevVotes) => {
-      return prevVotes.map((vote) =>
-        vote._id === newVote._id ? newVote : vote
-      );
-    });
   }, []);
 
-  const handleFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const filterValue = event.target.value;
-    setFilter(filterValue.toLowerCase());
-  };
+  const handleChangeVotes = React.useCallback(
+    (newVote: Vote) => {
+      setVotes((prevVotes) => {
+        const newVotes = prevVotes.map((vote) =>
+          vote._id === newVote._id ? newVote : vote
+        );
+        filterVotes(filter, newVotes);
+        return newVotes;
+      });
+    },
+    [filter, filterVotes, setVotes]
+  );
+
+  const handleFilter = React.useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const filterValue = event.target.value.toLowerCase();
+      setFilter(filterValue);
+      filterVotes(filterValue, votes);
+    },
+    [votes, filterVotes]
+  );
 
   if (loading) {
     return <LoadingSpinner />;
   }
 
-  const filteredVotes = votes.filter((vote) => {
-    const value = `${vote.artist} ${vote.album}`.toLowerCase();
-    return value.includes(filter);
-  });
-
   return (
-    <div>
+    <>
       <h1>Upravit hlasování</h1>
-      <TextField
+      <FilterTextField
         label="Filtrovat"
         variant="outlined"
         value={filter}
-        style={{ width: "50%", marginBottom: "50px" }}
         onChange={handleFilter}
       />
 
-      <ChangeList
-        users={users}
-        votes={filteredVotes}
-        onChangeVotes={handleChangeVotes}
-        onSaveVote={handleSaveVote}
-      />
-    </div>
+      {filteredVotes.map((vote) => (
+        <ChangeItem
+          key={vote._id}
+          users={users}
+          vote={vote}
+          onChangeVotes={handleChangeVotes}
+          onSaveVote={handleSaveVote}
+        />
+      ))}
+    </>
   );
 };
